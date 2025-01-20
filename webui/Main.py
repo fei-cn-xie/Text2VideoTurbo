a192b33cd5d7d627c3a051721ca52ee7f23c9ec8
Fei Xie <Fei.Xie1@ibm.com>
2025-01-20 10:20:43 +0800

    fix: 适配macOS M1pro

--- webui/Main.py
+++ webui/Main.py
@@ -5,7 +5,7 @@ from uuid import uuid4
 
 import streamlit as st
 from loguru import logger
-
+sys.path.append("/Users/xiefei/myspace/Learnspace/tech/gitrepo/Text2VideoTurbo")
 from app.config import config
 from app.models.const import FILE_TYPE_IMAGES, FILE_TYPE_VIDEOS
 from app.models.schema import MaterialInfo, VideoAspect, VideoConcatMode, VideoParams
